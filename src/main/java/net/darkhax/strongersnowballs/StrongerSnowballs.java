83d5e5c231a75848e52b8aca3a7814a127d65307
Tyler Hancock <darklime@live.ca>
2021-01-29 02:37:42 -0700

    Added debug info for mobs that are hurt by snowballs.

--- src/main/java/net/darkhax/strongersnowballs/StrongerSnowballs.java
+++ src/main/java/net/darkhax/strongersnowballs/StrongerSnowballs.java
@@ -1,6 +1,10 @@
 package net.darkhax.strongersnowballs;
 
+import java.util.List;
+
 import org.apache.commons.lang3.tuple.Pair;
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
 
 import net.minecraft.entity.EntityType;
 import net.minecraft.entity.LivingEntity;
@@ -9,8 +13,10 @@ import net.minecraft.potion.EffectInstance;
 import net.minecraft.potion.Effects;
 import net.minecraft.tags.EntityTypeTags;
 import net.minecraft.tags.ITag.INamedTag;
+import net.minecraftforge.client.event.RecipesUpdatedEvent;
 import net.minecraftforge.common.MinecraftForge;
 import net.minecraftforge.event.entity.living.LivingHurtEvent;
+import net.minecraftforge.eventbus.api.EventPriority;
 import net.minecraftforge.fml.ExtensionPoint;
 import net.minecraftforge.fml.ModLoadingContext;
 import net.minecraftforge.fml.common.Mod;
@@ -19,12 +25,14 @@ import net.minecraftforge.fml.network.FMLNetworkConstants;
 @Mod("strongersnowballs")
 public class StrongerSnowballs {
     
+	public static final Logger LOG = LogManager.getLogger("Stronger Snowballs");
     public static final INamedTag<EntityType<?>> HURT_BY_SNOW = EntityTypeTags.getTagById("strongersnowballs:hurt_by_snow");
     
     public StrongerSnowballs() {
         
         ModLoadingContext.get().registerExtensionPoint(ExtensionPoint.DISPLAYTEST, () -> Pair.of(() -> FMLNetworkConstants.IGNORESERVERONLY, (a, b) -> true));
         MinecraftForge.EVENT_BUS.addListener(this::onLivingHurt);
+        MinecraftForge.EVENT_BUS.addListener(EventPriority.LOWEST, this::onRecipesSynced);
     }
     
     private void onLivingHurt (LivingHurtEvent event) {
@@ -40,5 +48,13 @@ public class StrongerSnowballs {
                 event.setAmount(event.getAmount() + 1);
             }
         }
+    }
+    
+    private void onRecipesSynced (RecipesUpdatedEvent event) {
+        
+    	final List<EntityType<?>> validMobs = HURT_BY_SNOW.getAllElements();
+    	
+    	LOG.debug("The tag {} contained {} entries.", HURT_BY_SNOW.getName(), validMobs.size());
+    	validMobs.forEach(entry -> LOG.debug("Loaded {} as a valid target for mobs hurt by snowballs.", entry.getRegistryName()));
     }
 }
