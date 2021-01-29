9a7388f6d9289ee466c2e816ad2bb4fbb608e829
Tyler Hancock <darklime@live.ca>
2021-01-29 02:27:32 -0700

    Allow clients without this mod to connect on servers that do have it.

--- src/main/java/net/darkhax/strongersnowballs/StrongerSnowballs.java
+++ src/main/java/net/darkhax/strongersnowballs/StrongerSnowballs.java
@@ -1,5 +1,7 @@
 package net.darkhax.strongersnowballs;
 
+import org.apache.commons.lang3.tuple.Pair;
+
 import net.minecraft.entity.EntityType;
 import net.minecraft.entity.LivingEntity;
 import net.minecraft.entity.projectile.SnowballEntity;
@@ -9,7 +11,10 @@ import net.minecraft.tags.EntityTypeTags;
 import net.minecraft.tags.ITag.INamedTag;
 import net.minecraftforge.common.MinecraftForge;
 import net.minecraftforge.event.entity.living.LivingHurtEvent;
+import net.minecraftforge.fml.ExtensionPoint;
+import net.minecraftforge.fml.ModLoadingContext;
 import net.minecraftforge.fml.common.Mod;
+import net.minecraftforge.fml.network.FMLNetworkConstants;
 
 @Mod("strongersnowballs")
 public class StrongerSnowballs {
@@ -18,6 +23,7 @@ public class StrongerSnowballs {
     
     public StrongerSnowballs() {
         
+        ModLoadingContext.get().registerExtensionPoint(ExtensionPoint.DISPLAYTEST, () -> Pair.of(() -> FMLNetworkConstants.IGNORESERVERONLY, (a, b) -> true));
         MinecraftForge.EVENT_BUS.addListener(this::onLivingHurt);
     }
     
