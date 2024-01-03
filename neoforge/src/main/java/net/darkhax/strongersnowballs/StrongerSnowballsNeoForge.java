cb94567f017e77fa686134366b62b46a471675fe
Tyler Hancock <darklime@live.ca>
2024-01-03 03:27:25 -0700

    Port to MC 1.20.2 and add NeoForge support.

---
+++ neoforge/src/main/java/net/darkhax/strongersnowballs/StrongerSnowballsNeoForge.java
@@ -0,0 +1,13 @@
+package net.darkhax.strongersnowballs;
+
+import net.neoforged.fml.common.Mod;
+import net.neoforged.fml.loading.FMLPaths;
+
+@Mod(Constants.MOD_ID)
+public class StrongerSnowballsNeoForge {
+
+    public StrongerSnowballsNeoForge() {
+
+        new StrongerSnowballsCommon(FMLPaths.CONFIGDIR.get().resolve(Constants.MOD_ID + ".json").toFile());
+    }
+}
